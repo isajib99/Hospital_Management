652963a625b8de67697148a79aaf61ad38a03f8f
sajib <mmiah181047@bscse.uiu.ac.bd>
2022-06-29 23:56:07 +0600

    Final Update

---
+++ footer.php
@@ -0,0 +1,62 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>A concern of Care and Cure Group is the only multi-disciplinary super-specialty tertiary care hospital in Bangladesh. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p>United City, Satarkul, Dhaka</p>
+            <ul>
+              <li class="mobile">
+                <p>+8801711517350</p>
+                <p>1280</p>
+              </li>
+              <li class="email">
+                <p>rabbicse161@gmail.com</p>
+                <p>mrabbi161141@bscse.uiu.ac.bd</p>              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Hospital Management System - Template by M_ADNAN & Application Developed By <span style="color: white">Md.Fazle Rabbi & My team</span></p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
